a407ad5d0a9c2e4f1c5d30b7b051c670685d6256
wdarking <wdarking@gmail.com>
2016-04-04 19:33:39 -0300

    Fix mp_table with right indexes

--- string_compare/mp-program.f95
+++ string_compare/mp-program.f95
@@ -1,20 +1,31 @@
 program MP_ALGORITHM
 implicit none
 integer, parameter :: m=4
-character(LEN=m) :: x='abab'
-real, dimension(4) :: T
-integer :: i, j
+character(LEN=m) :: x='aaaa'
+integer, dimension(4) :: T
 
-i=0
-T(1)=-1
-j=-1
+call mp_table(x, m, T)
 
-do while(i < m)
-  do while((j > -1) .AND. (x(i+1:i+1) /= (x(j+i+1:j+i+1))))
-    j=T(1)
-  end do
-  i=i+1
-  j=j+1
-  T(i)=j
-end do
+contains
+
+	subroutine mp_table(x, m, T)
+	implicit none
+	integer, intent(in) :: m
+    character, intent(in) :: x
+	integer, dimension(m), intent(out) :: T
+	integer :: i, j
+		i=1
+		T(1)=0
+		j=0
+
+		do while(i < m)
+  			do while(j > 0 .and. x(i:i) /= x(j:j))
+    			j=T(j)
+  			end do
+  			i=i+1
+  			j=j+1
+  			T(i)=j
+		end do
+	end subroutine mp_table
+    
 end  program MP_ALGORITHM
